7cf701bb24a74da2f23c579ba49eecc89a329b6c
Omar Afifi <omarafifi.cse@gmail.com>
2025-09-17 18:05:52 +0300

    create appassets class

---
+++ lib/styling/app_assets.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  // Example:
+  // static const String onboardingImage = 'assets/images/Onboarding.png';
+  // static const String homeIcon = 'assets/icons/Home.svg';
+  // static const String profileIcon = 'assets/icons/Profile.svg';
+}
